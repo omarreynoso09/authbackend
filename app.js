5e8c5954bdac91608cbf06ff20c889115eaaf2c4
Hansel Reynoso <omarreynoso09@gmail.com>
2022-07-27 11:14:37 -0400

    fixed bug

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var usersRouter = require("./routes/users");
 var authRouter = require("./routes/auth");
 
 var app = express();
+
 var { mongoConnect } = require("./mongo.js");
 mongoConnect();
 
@@ -38,11 +39,9 @@ app.use(function (req, res, next) {
 
 // error handler
 app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get("env") === "development" ? err : {};
 
-  // render the error page
   res.status(err.status || 500);
   res.render("error");
 });
--- routes/auth.js
+++ routes/auth.js
@@ -3,44 +3,46 @@ var router = express.Router();
 const bcrypt = require("bcryptjs");
 const { uuid } = require("uuidv4");
 const { blogsDB } = require("../mongo");
-const { MongoMissingCredentialsError } = require("mongodb");
+
 const dotenv = require("dotenv");
 const jwt = require("jsonwebtoken");
 dotenv.config();
 
 const createUser = async (username, passwordHash) => {
   const collection = await blogsDB().collection("users");
+
   const user = {
     username: username,
     password: passwordHash,
     uid: uuid(),
   };
+  console.log("mongo response " + collection);
   try {
     await collection.insertOne(user);
     return true;
   } catch (error) {
-    console.error(e);
+    console.log("something is wrong");
+    console.error(error);
     return false;
   }
 };
 
-router.post("/register-user", async function (req, res, next) {
+router.post("/register-user", async (req, res, next) => {
   try {
     const username = req.body.username;
     const password = req.body.password;
-    const saltRounds = 5; // In a real application, this number would be somewhere between 5 and 10
+    const saltRounds = 5;
     const salt = await bcrypt.genSalt(saltRounds);
     const hash = await bcrypt.hash(password, salt);
     const userSaveSuccess = await createUser(username, hash);
-    res.status(200).json({ success: userSaveSuccess });
+
+    res.json({ success: userSaveSuccess }).status(200);
   } catch (e) {
-    res
-      .status(500)
-      .json({ message: "Error registering user.", success: false });
+    res.json({ success: false }).status(500);
   }
 });
 
-router.post("/login-user", async function (req, res, next) {
+router.post("/login-user", async (req, res, next) => {
   const username = req.body.username;
   const password = req.body.password;
   const collection = await blogsDB().collection("users");
@@ -50,7 +52,6 @@ router.post("/login-user", async function (req, res, next) {
     });
     if (!user) {
       res.json({ success: false }).status(204);
-      return;
     }
     const match = await bcrypt.compare(password, user.password);
     if (match) {
@@ -59,22 +60,26 @@ router.post("/login-user", async function (req, res, next) {
         time: new Date(),
         userId: user.uid,
       };
+
       const token = jwt.sign(data, jwtSecretKey);
-      res.status(200).json({ success: true, token });
+      res.json({ success: true, token }).status(200);
       return;
     }
     res.json({ success: false });
   } catch (error) {
-    res.status(500).json({ message: "Error logging in.", success: false });
+    console.log(error);
+    res.json({ message: "Error Logging In.", success: false }).status(500);
   }
 });
 
-router.get("/validate-token", function (req, res) {
+router.get("/validate-token", async (req, res, next) => {
   const tokenHeaderKey = process.env.TOKEN_HEADER_KEY;
   const jwtSecretKey = process.env.JWT_SECRET_KEY;
+
   try {
     const token = req.header(tokenHeaderKey);
     const verified = jwt.verify(token, jwtSecretKey);
+
     if (verified) {
       return res.json({ success: true });
     } else {
@@ -83,7 +88,7 @@ router.get("/validate-token", function (req, res) {
     }
   } catch (error) {
     // Access Denied
-    return res.status(401).json({ success: true, message: String(error) });
+    return res.status(401).json({ success: error, message: String(error) });
   }
 });
 
